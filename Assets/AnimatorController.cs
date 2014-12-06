bc9c056f5df7dc326bdab9af1f4d11c4f0c73a13
Lucien Lee <lkiral7903@gmail.com>
2014-12-07 02:43:08 +0800

    add animator controller

---
+++ Assets/AnimatorController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatorController : MonoBehaviour {
+
+	public Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = gameObject.GetComponent<Animator> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		animator.SetFloat ("velocity", this.rigidbody.velocity.magnitude);
+	}
+}
