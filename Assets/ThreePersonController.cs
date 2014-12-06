92905bcd6f2535b162318ae36159fdcc1e99f5b8
Lucien Lee <lkiral7903@gmail.com>
2014-12-07 02:43:43 +0800

    clean code

--- Assets/ThreePersonController.cs
+++ Assets/ThreePersonController.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class ThreePersonController : MonoBehaviour {
 
 	public GameObject player;
-	public GameObject camera;
+	public GameObject mainCamera;
 	public GameObject cameraCollisionBox;
 	public CollisionCounter probe;
 	public CollisionCounter cameraProbe;
@@ -14,18 +14,42 @@ public class ThreePersonController : MonoBehaviour {
 	public float verticalRatio;
 	
 
-	void CalculateHeight(){
-		Vector3 originVector = camera.transform.position;
-		Vector3 localVector = camera.transform.localPosition;
+	private bool isHoriMove;
+	private bool isVertiMove;
+	private Vector3 horiVelocity;
+	private Vector3 vertiVelocity;
 
-		// REMOVE Y FACTOR IN magnitude
+	//FOR RESET CHARACTOR FLOAT IN AIR
+	private float originYVelocity; 
+
+	void CalculateHeight(){
+		Vector3 originVector = mainCamera.transform.position;
+		Vector3 localVector = mainCamera.transform.localPosition;
+
+		// REMOVE Y to caculate XZ magnitude
 		originVector.y = player.transform.position.y;
 		
 		float y = Maxdistance - (originVector - player.transform.position).magnitude;
 		localVector.y=y;
-		camera.transform.localPosition = localVector*verticalRatio;
+		mainCamera.transform.localPosition = localVector*verticalRatio;
 	}
-	
+
+	void Draw(){
+		if( this.isHoriMove && this.isVertiMove ){
+			player.rigidbody.velocity = this.horiVelocity + this.vertiVelocity;
+		}else if( this.isHoriMove ){
+			player.rigidbody.velocity = this.horiVelocity;
+		}else if( this.isVertiMove ){
+			player.rigidbody.velocity = this.vertiVelocity;
+		}
+		// Caculate the player's degree from velocity 
+		if (this.isHoriMove || this.isVertiMove) {
+			float rotate = Mathf.Atan2 (player.rigidbody.velocity.x, player.rigidbody.velocity.z);
+			player.transform.rotation = Quaternion.Euler (0, rotate / Mathf.PI * 180, 0);
+		}
+		// Reset Y axis velocity to origin
+		player.rigidbody.velocity = new Vector3 (player.rigidbody.velocity.x, this.originYVelocity, player.rigidbody.velocity.z);
+	}
 
 	// Update is called once per frame
 	void Update () {
@@ -33,26 +57,29 @@ public class ThreePersonController : MonoBehaviour {
 		float vertical = Input.GetAxis("Vertical");
 		float horizontal = Input.GetAxis("Horizontal");
 		// GET DIRECTION
-		Vector3 forward = camera.transform.forward;
-		Vector3 right = camera.transform.right;
-
-		//KEEP Y AXIX 
+		Vector3 forward = mainCamera.transform.forward;
+		Vector3 right = mainCamera.transform.right;
+		//Calibrate Y AXIS 
 		forward.y = 0;
 		right.y = 0;
 		// GET DIRECTION UNIT VECTOR
 		forward.Normalize();
 		right.Normalize ();
 
-		//FOR RESET CHARACTOR FLOAT IN AIR
-		float Yvelocity = player.rigidbody.velocity.y;
-		Vector3 distance = (  camera.transform.position - player.transform.position );
+		Vector3 distance = ( mainCamera.transform.position - player.transform.position );
 		distance.y = 0;
 
-		if (vertical > 0) {
-			player.transform.rotation = Quaternion.Euler(0,camera.transform.rotation.eulerAngles.y,0);
-			player.rigidbody.velocity = forward*moveSpeed;
+		//RESET ROTATION 
+		this.isHoriMove = false;
+		this.isVertiMove = false;
+		this.horiVelocity = Vector3.zero;
+		this.vertiVelocity = Vector3.zero;
 
-			// chase player 
+		this.originYVelocity = player.rigidbody.velocity.y;
+
+		if (vertical > 0) {
+			this.vertiVelocity = forward*moveSpeed;
+			//Camera chases the player
 			if( distance.magnitude > Maxdistance ){
 				float originY = cameraCollisionBox.transform.position.y;
 				Vector3 newPosition = player.transform.position + distance.normalized * Maxdistance;
@@ -61,43 +88,40 @@ public class ThreePersonController : MonoBehaviour {
 			}
 
 			this.CalculateHeight();
+			isVertiMove = true;
 
 		}else if( vertical < 0){
-			player.transform.rotation = Quaternion.Euler(0,camera.transform.rotation.eulerAngles.y+180,0);
-			player.rigidbody.velocity = -forward*moveSpeed;
-
+			this.vertiVelocity = -forward*moveSpeed;
 			if( cameraProbe.counter <= 0 ){
-				// chase player 
+				//Camera chases the player
 				float originY = cameraCollisionBox.transform.position.y;
 				Vector3 newPosition = player.transform.position + distance.normalized * Maxdistance;
 				newPosition.y = originY;
 				cameraCollisionBox.transform.position = newPosition;
 			}else{
-				// camera up to sky
+				//Camera move higher to see the player
 				this.CalculateHeight();
 			}
-
-		}else{
-			player.rigidbody.velocity = Vector3.zero;
+			isVertiMove = true;
 		}
 
 		if (horizontal > 0){
-			player.transform.rotation = Quaternion.Euler(0,camera.transform.rotation.eulerAngles.y+90,0);
 			if( probe.counter > 0 ){
 				cameraCollisionBox.transform.position += -right*moveSpeed*Time.deltaTime;
-			}else{ 
-				player.rigidbody.velocity = right*moveSpeed;
+			}else{
+				this.horiVelocity = right*moveSpeed;
 			}
+			isHoriMove = true;
 		}else if (horizontal < 0){
-			player.transform.rotation = Quaternion.Euler(0,camera.transform.rotation.eulerAngles.y-90,0);
 			if( probe.counter > 0 ){
 				cameraCollisionBox.transform.position += right*moveSpeed*Time.deltaTime;
 			}else{
-				player.rigidbody.velocity = -right*moveSpeed;
+				this.horiVelocity = -right*moveSpeed;
 			}
+			isHoriMove = true;
 		}
 
-		//RESET CHARACTOR FLOAT IN AIR
-		player.rigidbody.velocity = new Vector3 (player.rigidbody.velocity.x, Yvelocity, player.rigidbody.velocity.z);
+		// perform the result on charactor
+		Draw ();
 	}
 }
